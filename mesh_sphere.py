4feaa97ba8a87654ac4411ad0cedaaafdb6898e2
Gossfunkel <katherineegoss@gmail.com>
2026-07-31 16:27:54 +0100

    successful mesh sphere

---
+++ mesh_sphere.py
@@ -0,0 +1,148 @@
+from direct.showbase.ShowBase import ShowBase
+from direct.filter.FilterManager import FilterManager
+from panda3d.core import (
+    load_prc_file_data, NodePath, Vec3, GeomNode, Geom, GeomEnums, ModelRoot,
+    GeomVertexFormat, GeomVertexData, GeomVertexWriter, GeomTriangles, 
+    BoundingVolume, BoundingBox, ComputeNode, ColorBlendAttrib, CardMaker,
+    Shader, ShaderBuffer, Texture, SamplerState, ShaderAttrib
+)
+import numpy as np
+
+TAU = 6.2831853
+
+CONFIG = """
+win-size 1920 1040
+gl-version 4 3
+load-display pandagl
+gl-debug true
+gl-debug-buffers true
+gl-force-glsl-version 430 // required for ssbo format
+framebuffer-srgb true
+hardware-animated-vertices true
+"""
+load_prc_file_data('', CONFIG)
+
+if __name__ == "__main__":
+    ShowBase()
+    base.set_background_color(0.,0.,0.,1.)
+
+    sphere_rad = 32
+    num_stacks = 10
+    num_sectors = 10
+    sector_len = TAU / num_sectors;
+    stack_len = (TAU/2.) / num_stacks;
+    num_verts = num_stacks + (num_stacks - 2) * (num_sectors - 1)
+    raw_ssbo_data = np.zeros(4*num_verts, dtype=np.float32)
+
+    idx = 1
+    for stack in range(num_stacks):
+        theta = TAU / 4. - stack * stack_len;
+        xy = sphere_rad * np.cos(theta)
+        zpos = sphere_rad * np.sin(theta);
+        if (stack == 0):
+            raw_ssbo_data[0] = 0.
+            raw_ssbo_data[1] = 0.
+            raw_ssbo_data[2] = zpos
+        elif (stack == num_stacks - 1):
+            raw_ssbo_data[(num_verts-1)*4] = 0.
+            raw_ssbo_data[(num_verts-1)*4 + 1] = 0.
+            raw_ssbo_data[(num_verts-1)*4 + 2] = zpos
+        else:
+            for sector in range(num_sectors):
+                phi = sector * sector_len;
+                raw_ssbo_data[idx*4] = xy * np.cos(phi); # r * cos(u) * cos(v)
+                raw_ssbo_data[idx*4+1] = xy * np.sin(phi);
+                raw_ssbo_data[idx*4+2] = zpos
+                idx += 1
+
+    ssbo = ShaderBuffer('sprites', raw_ssbo_data.tobytes(), GeomEnums.UHStatic)
+
+    vtx_format = GeomVertexFormat.get_empty()
+    vtx_data = GeomVertexData('sprites', vtx_format, GeomEnums.UH_static)
+
+    #num_tris = (num_sectors-2)*2*(num_stacks-2) + num_stacks*2
+    geom_tris = GeomTriangles(GeomEnums.UH_static)
+    for stack in range(num_stacks-1):
+        for sector in range(num_sectors):
+            if (stack == 0):
+                geom_tris.add_vertex(0)
+                geom_tris.add_vertex(1 + sector)
+                if (sector == num_sectors-1):
+                    geom_tris.add_vertex(1)
+                else:
+                    geom_tris.add_vertex(2 + sector)
+            elif (stack == num_stacks-2):
+                geom_tris.add_vertex(num_verts-1)
+                geom_tris.add_vertex(1 + (stack-1)*num_sectors + sector)
+                if (sector == num_sectors-1):
+                    geom_tris.add_vertex(1 + (stack-1)*num_sectors)
+                else:
+                    geom_tris.add_vertex(2 + (stack-1)*num_sectors + sector)
+            else:
+                geom_tris.add_vertex(1 + (stack-1)*num_sectors + sector)
+                if (sector == num_sectors-1):
+                    geom_tris.add_vertex(1 + (stack-1)*num_sectors)
+                else:
+                    geom_tris.add_vertex(2 + (stack-1)*num_sectors + sector)
+                geom_tris.add_vertex(1 + stack*num_sectors + sector)
+                geom_tris.add_vertex(1 + stack*num_sectors + sector)
+                if (sector == num_sectors-1):
+                    geom_tris.add_vertex(1 + (stack-1)*num_sectors)
+                    geom_tris.add_vertex(1 + stack*num_sectors)
+                else:
+                    geom_tris.add_vertex(2 + (stack-1)*num_sectors + sector)
+                    geom_tris.add_vertex(2 + stack*num_sectors + sector)
+
+    geom = Geom(vtx_data)
+    geom.add_primitive(geom_tris)
+    geom.set_bounds(BoundingBox((-1, -1, -1), (100, 100, 100)))
+
+    geom_node = GeomNode("gnode")
+    geom_node.add_geom(geom)
+
+    mesh_shader = Shader.load(Shader.SL_GLSL, "mesh.vert", "mesh.frag")
+    mesh_np = base.render.attach_new_node(geom_node)
+    mesh_np.set_shader(mesh_shader)
+    mesh_np.set_shader_input("vert_buff", ssbo)
+    mesh_np.set_two_sided(True)
+    mesh_np.set_attrib(ColorBlendAttrib.make(ColorBlendAttrib.M_add, ColorBlendAttrib.O_incoming_alpha, ColorBlendAttrib.O_one))
+    mesh_np.set_depth_write(False)
+    mesh_np.node().set_bounds_type(BoundingVolume.BT_box)
+
+    compute_node = ComputeNode("compute")
+    compute_node.add_dispatch(num_verts // 64, 4, 1)
+    compute_np = base.render.attach_new_node(compute_node)
+    compute_np.set_shader(Shader.load_compute(Shader.SL_GLSL, "mesh_sphere.comp"))
+    compute_np.set_shader_input("vert_buff", ssbo)
+    compute_np.set_shader_input("num_verts", num_verts)
+
+    # filter_mgr = FilterManager(base.win, base.cam)
+    # #filter_mgr.resizeBuffers()
+    # #filter_mgr.windowEvent(base.win)
+    # screen_tex = Texture()
+    # screen_tex.setMagfilter(SamplerState.FT_nearest)
+    # screen_tex.setMinfilter(SamplerState.FT_nearest)
+    # #screen_tex.setMatchFramebufferFormat()
+    # screen_card = filter_mgr.renderSceneInto(colortex=screen_tex)
+    # screen_tex.set_format(Texture.F_srgb_alpha)
+    # screen_card.set_shader(Shader.load(Shader.SL_GLSL, vertex="quad.vert", fragment="screen_filter.frag"))
+    # screen_card.set_shader_input("screen_scale", base.win.properties.getSize())
+    # screen_card.set_shader_input("screen_tex", screen_tex)
+    # #screen_card.set_attrib(ColorBlendAttrib.make(ColorBlendAttrib.M_add, ColorBlendAttrib.O_one , ColorBlendAttrib.O_one_minus_incoming_alpha))
+    # screen_card.set_attrib(ColorBlendAttrib.make(ColorBlendAttrib.M_add, ColorBlendAttrib.O_incoming_alpha , ColorBlendAttrib.O_one))
+    #base.win.set_clear_color_active(True)
+    
+    base.accept("escape", base.userExit)
+    
+    def rotate_cam(task):
+        base.cam.set_pos(np.sin(2.*np.pi/3.+task.frame/200.)*sphere_rad*6.,
+            -np.cos(2.*np.pi+task.frame/400.)*sphere_rad*6.,np.cos(task.frame/800.)*3. + 2.)
+        base.cam.look_at((0., 0., 1.))
+        return task.cont
+
+    base.taskMgr.add(rotate_cam, "rotate-camera")
+
+    #base.cam.set_pos(0., -6. * sphere_rad, 5.)
+    #base.cam.look_at(0., 0., 1.)
+
+    base.run()
